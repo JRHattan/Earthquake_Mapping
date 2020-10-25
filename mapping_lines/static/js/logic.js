17a837553c6110e7b62e6aaa2ea762a61ab77a3f
JRHattan <68794172+JRHattan@users.noreply.github.com>
2020-10-25 16:43:00 -0500

    Update logic.js

--- mapping_lines/static/js/logic.js
+++ mapping_lines/static/js/logic.js
@@ -2,7 +2,7 @@
 console.log("working");
 
 // Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([36.1733, -120.1794], 7);
+let map = L.map('mapid').setView([37.6213, -122.3790], 5);
 // Coordinates for each point to be used in the polyline.
 let line = [
   [33.9416, -118.4085],
@@ -10,9 +10,9 @@ let line = [
   [40.7899, -111.9791],
   [47.4502, -122.3088]
 ];
-// Create a polyline using the line coordinates and make the line red.
+// Create a polyline using the line coordinates and make the line black.
 L.polyline(line, {
-  color: "red"
+  color: "yellow"
 }).addTo(map);
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
