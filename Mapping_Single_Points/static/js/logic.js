2257339c4c595b6ba5d62fac996a56716c05c269
JRHattan <68794172+JRHattan@users.noreply.github.com>
2020-10-25 15:51:33 -0500

    Update logic.js

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -13,9 +13,9 @@ let cityData = cities;
 cityData.forEach(function(city) {
   console.log(city)
   L.circleMarker(city.location, {
-    Radius: city.population,
+    Radius: city.population/1000000,
   })
-  .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population + "</h3>")
+  .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
   .addTo(map);
 });
 // We create the tile layer that will be the background of our map.
