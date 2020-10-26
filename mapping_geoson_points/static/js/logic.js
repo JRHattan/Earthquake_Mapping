1b9a03d5f30b77a198b19e570ed7ddce4199d509
JRHattan <68794172+JRHattan@users.noreply.github.com>
2020-10-25 20:07:36 -0500

    Update logic.js

--- mapping_geoson_points/static/js/logic.js
+++ mapping_geoson_points/static/js/logic.js
@@ -1,7 +1,7 @@
 // Add console.log to check to see if our code is working.
 console.log("working");
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let light = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
   attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
   maxZoom: 18,
   accessToken: API_KEY
@@ -33,8 +33,8 @@ L.control.layers(baseMaps).addTo(map);
 // Accessing the airport GeoJSON URL
 let airportData = "https://raw.githubusercontent.com/JRHattan/Earthquake_Mapping/main/majorAirports.json";
 // Grabbing our GeoJSON data.
-d3.json(airportData).then(function(data) {
-    console.log(data);
+d3.json(torontoData).then(function(data) {
+  console.log(data);
 // Creating a GeoJSON layer with the retrieved data.
-  L.geoJson(data).addTo(map);
+L.geoJson(data).addTo(map);
 });
