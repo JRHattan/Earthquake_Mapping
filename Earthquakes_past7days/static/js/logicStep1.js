7a9d7a9eff9555e1c75864cf35abd12f0c52f12a
JRHattan <68794172+JRHattan@users.noreply.github.com>
2020-10-25 21:02:50 -0500

    Update logicStep1.js

--- Earthquakes_past7days/static/js/logicStep1.js
+++ Earthquakes_past7days/static/js/logicStep1.js
@@ -1,5 +1,6 @@
 // Add console.log to check to see if our code is working.
 console.log("working");
+
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
   attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -30,11 +31,6 @@ let map = L.map('mapid', {
 // Pass our map layers into our layers control and add the layers control to the map.
 L.control.layers(baseMaps).addTo(map);
 
-// Accessing the Toronto neighborhoods GeoJSON URL.
-//let torontoHoods = "https://raw.githubusercontent.com/JRHattan/Mapping_Earthquakes/main/torontoNeighborhoods.json";// Create a style for the lines.
-
-// Grabbing our GeoJSON data.
-// Retrieve the earthquake GeoJSON data.
 d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function(data) {
   // Creating a GeoJSON layer with the retrieved data.
   L.geoJson(data).addTo(map);
